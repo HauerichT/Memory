37bd2d50fd188dc64f7cc1d5920a990e61817860
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-11 12:44:41 +0100

    Initial

--- src/Memory.java
+++ src/Memory.java
@@ -1,23 +1,49 @@
 public class Memory {
 
     public int[][] generateField(int n) {
+
+        // prüft, ob n gerade ist
+        if (n % 2 != 0) {
+            System.out.println("n muss gerade sein.");
+            System.exit(0);
+        }
+
+        // erzeugt das Spielfeld
         int[][] field = new int[n][n];
+
+        // Variablen zur Speicherung der Positionen im Spielfeld
         int zufallszahlZeile = 0;
         int zufallszahlSpalte = 0;
 
-        for (int i = 5; i < (n*n)+5; i++) {
-            zufallszahlZeile = (int) (Math.random() * n);
-            zufallszahlSpalte = (int) (Math.random() * n);
+        // Counter für die Zahlen der Spielfelder
+        int numberInField = 5;
 
-            field[zufallszahlZeile][zufallszahlSpalte] = i;
+        // weist jedem Feld Wert zu
+        // (n*n)/2 -> Anzahl der Felder durch 2, weil Felder paarweise besetzt werden
+        for (int i = 0; i < (n*n)/2; i++) {
+
+            // weist Feldern paarweise Werte >= 5 zu
+            for (int j = 0; j < 2; j++) {
+
+                // solange ein bereits belegtes Feld gefunden wird, wird ein freies gesucht
+                while (field[zufallszahlZeile][zufallszahlSpalte] != 0) {
+                    zufallszahlZeile = (int) (Math.random() * n);
+                    zufallszahlSpalte = (int) (Math.random() * n);
+                }
+
+                // weist Feld den ermittelten Wert zu
+                field[zufallszahlZeile][zufallszahlSpalte] = numberInField;
+
+            }
+            // erhöht den Counter für die Zahlenwerte um 1
+            numberInField++;
         }
 
-        System.out.println(zufallszahlZeile);
-        System.out.println(zufallszahlSpalte);
-
+        // gibt das Spielfeld zurück
         return field;
     }
 
+    // gibt das Spielfeld auf der Konsole aus
     public void showField(int[][] field) {
         for (int[] ints : field) {
             for (int anInt : ints) {
--- src/MemoryTest.java
+++ src/MemoryTest.java
@@ -1,7 +1,19 @@
 public class MemoryTest {
     public static void main(String[] args) {
-        Memory memory = new Memory();
-        int[][] field = memory.generateField(3);
-        memory.showField(field);
+
+        // Test n=4
+        Memory memory4 = new Memory();
+        int[][] field4 = memory4.generateField(4);
+        System.out.println("Test n=4:");
+        memory4.showField(field4);
+
+        // spacer
+        System.out.println("----------");
+
+        // Test n=6
+        Memory memory6 = new Memory();
+        int[][] field6 = memory6.generateField(6);
+        System.out.println("Test n=6:");
+        memory6.showField(field6);
     }
 }
